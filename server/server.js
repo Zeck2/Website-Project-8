ea8d6ad0d959d3e148622f5d12b476c83254e7b7
Zeck2 <118732833+Zeck2@users.noreply.github.com>
2023-08-22 23:33:35 +0800

    updated server.js

--- server/server.js
+++ server/server.js
@@ -1,22 +1,16 @@
-//import modules
 const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const cors = require("cors");
+const testRoutes = require("./routes/test");
 require("dotenv").config();
 
-//app
+
 const app = express();
-
-//port
-const port = process.env.PORT || 3000;
-
-//listener
 const server = app.listen(port, () =>
   console.log(`Niggana ang server sa port ${port}`)
 );
 
-//database
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -25,10 +19,6 @@ mongoose
   .then(() => console.log("CONNECTED IMONG DATABASE"))
   .catch((error) => console.log("HALA! IMONG DATABASE NAG ERROR", error));
 
-//middleware
 app.use(morgan("dev"));
 app.use(cors({ origin: true, credentials: true }));
-
-//routes
-const testRoutes = require("./routes/test");
 app.use("/api", testRoutes);
